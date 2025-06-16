ffa133c381d153c666490b2740adf98e1330c66f
thiago <thiagoc.sousa@hotmail.com>
2025-06-16 18:34:19 -0300

    creating Menu class connecting Menu and game classes

--- code/Game.py
+++ code/Game.py
@@ -1,9 +1,26 @@
-class Game:
-    def inicia(self):
-        pass
+import pygame
 
-    def loop(self):
-        pass
+from code.Menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+            # check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()  # end pygme
+
+
+
 
     def atualiza_tela(self):
         pass
